976273391353913e5df21a1856cd9dbea12ea6af
venkat <v.kannan@infocerv.com>
2022-03-16 21:50:07 +0530

    implement simple recyclerview

---
+++ app/src/main/java/com/venkat/recyclerviewdemo/Friend.kt
@@ -0,0 +1,6 @@
+package com.venkat.recyclerviewdemo
+
+data class Friend(
+    val name: String,
+    val age: Int
+)
---
+++ app/src/main/java/com/venkat/recyclerviewdemo/MyRecyclerViewAdapter.kt
@@ -0,0 +1,36 @@
+package com.venkat.recyclerviewdemo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.concurrent.fixedRateTimer
+
+class MyRecyclerViewAdapter(private val friends:List<Friend>,private val clickListener : (Friend)->Unit) : RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val listItem = layoutInflater.inflate(R.layout.list_item,parent,false)
+        return MyViewHolder(listItem)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val friend = friends[position]
+        holder.bind(friend,clickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return friends.size
+    }
+}
+
+class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view){
+    val nameTextView = view.findViewById<TextView>(R.id.name_text_view)
+    fun bind(friend: Friend,clickListener: (Friend) -> Unit){
+        nameTextView.text = "Hello ${friend.name},${friend.age}"
+        view.setOnClickListener {
+            clickListener(friend)
+        }
+    }
+}
